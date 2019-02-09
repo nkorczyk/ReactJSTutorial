b9c8df5a93873bd854d4bd8f7c58a6e19c52d62c
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-02-09 19:52:40 +0100

    Data preparation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => (
-  <div>
-    <h1>Init</h1>
-  </div>
-);
+import App from './App';
 
 ReactDOM.render(
   <App />,
