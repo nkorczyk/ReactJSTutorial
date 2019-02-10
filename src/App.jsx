e5f06f4a216d04fd2dc1c4d7493295d7c4914398
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-02-10 15:08:45 +0100

    Addidng new item to the list

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,22 @@ class App extends React.Component {
 
   handleAddItem = (event) => {
     event.preventDefault();
-    console.log(event.target[0].value);
-    console.log(event.target[1].value);
-    console.log(event.target[2].value);
-    console.log(event.target[3].value);
+
+    const newItem = {
+      name: event.target[0].value,
+      twitterLink: event.target[1].value,
+      image: event.target[2].value,
+      description: event.target[3].value,
+    };
+
+    this.setState(prevState => ({
+      items: [
+        ...prevState.items,
+        newItem,
+      ],
+    }));
+
+    event.target.reset();
   }
 
   render() {
