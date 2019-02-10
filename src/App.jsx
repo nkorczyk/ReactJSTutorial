44728f4105778abbf0637b8a4ef92d2217768209
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-02-10 14:56:57 +0100

    Form styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,8 @@
 import React from 'react';
 import ListWrapper from './components/ListWrapper/ListWrapper';
+import initialState from './constants/initialState';
+import Form from './components/Form/Form';
 import './index.css';
-
-const initialState = [
-  {
-    image: 'https://www.tate.org.uk/art/images/research/2148_10.jpg',
-    name: 'Dan Abramov',
-    description: 'Working on @reactjs. The demo guy.',
-    twitterLink: 'https://twitter.com/dan_abramov',
-  },
-  {
-    image: 'https://www.tate.org.uk/art/images/research/2148_10.jpg',
-    name: 'Ryan Florence',
-    description: 'Making React accessible for users and developers at https://reach.tech . Online learning, workshops, OSS, and consulting.',
-    twitterLink: 'https://twitter.com/ryanflorence',
-  },
-  {
-    image: 'https://www.tate.org.uk/art/images/research/2148_10.jpg',
-    name: 'Michael Jackson',
-    description: 'Maker. Co-author of React Router. Working on @ReactTraining. Created @unpkg. Head over heels for @cari.',
-    twitterLink: 'https://twitter.com/mjackson',
-  },
-  {
-    image: 'https://www.tate.org.uk/art/images/research/2148_10.jpg',
-    name: 'Kent C. Dodds',
-    description: 'Making software development more accessible · Husband, Father, Latter-day Saint, Teacher, OSS, GDE, @TC39 · @PayPalEng @eggheadio @FrontendMasters · #JS',
-    twitterLink: 'https://twitter.com/kentcdodds',
-  },
-];
 
 class App extends React.Component {
   state = {
@@ -36,11 +11,21 @@ class App extends React.Component {
     ],
   };
 
+  handleAddItem = (event) => {
+    event.preventDefault();
+    console.log(event.target[0].value);
+    console.log(event.target[1].value);
+    console.log(event.target[2].value);
+    console.log(event.target[3].value);
+  }
+
   render() {
     return (
       <div>
         <ListWrapper
           items={this.state.items} />
+        <Form
+          submitFn={this.handleAddItem} />
       </div>
     );
   }
