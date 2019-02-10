d1c06c58be41d254bddf14d33131ea42ad051190
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-02-10 13:06:33 +0100

    Class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ListWrapper from './components/ListWrapper/ListWrapper';
+import MyComponent from './components/MyComponent/MyComponent';
 import './index.css';
 
 const App = () => (
   <div>
+    <MyComponent />
     <ListWrapper />
   </div>
 );
---
+++ src/components/MyComponent/MyComponent.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+  state = {
+    text: "test state"
+  };
+
+  handleChange = (event) => (
+    this.setState({
+        text: event.target.value.toUpperCase()
+    })
+  );
+
+  render() {
+    return (
+      <>
+        <input
+          placeholder="your text"
+          onChange={this.handleChange}
+          value={this.state.text}
+        />
+        <h1>{this.state.text}</h1>
+      </>
+    );
+  }
+}
+
+export default MyComponent;
